6f96240342e1880086ed232abfc82cbd6507e0d2
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-08 09:15:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,8 @@
 def map_to_negative
   array= [1,2,3,-9]
-  map_to_negative([1,2,3,-9])
   map([1,2,3,-9]) {|n| n*-1}
 end
+
+def map_to_no_change
+  dune= ["paul", "gurney", "vladimir", "jessica", "chani"]
+  map_to_no_change(["paul", "gurney", "vladimir", "jessica", "chani"])
