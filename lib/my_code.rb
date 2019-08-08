52d7eed24f941f64d673ad266a1bdb70341bedf1
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-08 09:20:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,3 +6,9 @@ end
 def map_to_no_change
   dune= ["paul", "gurney", "vladimir", "jessica", "chani"]
   map(["paul", "gurney", "vladimir", "jessica", "chani"]) {|n| n=dune}
+end
+
+def map_to_double
+  array= [1,2,3,-9]
+  map(array){|n| n*2}
+end
